2827101592c20f2edcc6714265391b8c85a29892
Gabriel Ferrari <gabrielferrari446@gmail.com>
2024-12-10 16:26:19 -0300

    Making the code responsive(calculate mixture)

--- src/components/CoolPropForm.jsx
+++ src/components/CoolPropForm.jsx
@@ -24,12 +24,21 @@ const CoolPropForm = ({
   setProportion2,
   onCalculate,
 }) => {
-  const [isMixture, setIsMixture] = useState(false); // Estado para a checkbox
+  const [isMixture, setIsMixture] = useState(false); 
 
   return (
     <div>
       <h2>Parâmetros de Entrada</h2>
-      {/* Checkbox para mistura */}
+
+      <div>
+        <label>Nome do fluido 1:</label>
+        <Select
+          options={fluids.map((fluid) => ({ value: fluid, label: fluid }))}
+          value={{ value: fluidName, label: fluidName }}
+          onChange={(selectedOption) => setFluidName(selectedOption.value)}
+        />
+      </div>
+
       <div>
         <label>
           <input
@@ -41,16 +50,19 @@ const CoolPropForm = ({
         </label>
       </div>
 
-      {/* Dropdown para selecionar unidade: Massa ou Molar */}
-      <div>
-        <label>Unidade de cálculo:</label>
-        <select value={unitType} onChange={(e) => setUnitType(e.target.value)}>
-          <option value="mass">Massa</option>
-          <option value="mol">Mol</option>
-        </select>
-      </div>
+      {isMixture && (
+        <div className="unit-type">
+          <label>Unidade de cálculo:</label>
+          <select value={unitType} onChange={(e) => setUnitType(e.target.value)}>
+            <option value="mass">Massa</option>
+            <option value="mol">Mol</option>
+          </select>
+        </div>
+      )}
 
-      {/* Substância 1 */}
+      {isMixture && (
+  <div className="mixture-fields">
+    <div className="mixture-pair">
       <div>
         <label>Nome do fluido 1:</label>
         <Select
@@ -59,45 +71,40 @@ const CoolPropForm = ({
           onChange={(selectedOption) => setFluidName(selectedOption.value)}
         />
       </div>
+      <div>
+        <label>Proporção do fluido 1:</label>
+        <input
+          type="number"
+          value={proportion1}
+          onChange={(e) => setProportion1(e.target.value)}
+          placeholder="Proporção do fluido 1"
+        />
+      </div>
+    </div>
 
-      {/* A proporção do fluido 1 aparece apenas se a checkbox for marcada */}
-      {isMixture && (
-        <div>
-          <label>Proporção do fluido 1:</label>
-          <input
-            type="number"
-            value={proportion1}
-            onChange={(e) => setProportion1(e.target.value)}
-            placeholder="Proporção"
-          />
-        </div>
-      )}
+    <div className="mixture-pair">
+      <div>
+        <label>Nome do fluido 2:</label>
+        <Select
+          options={fluids.map((fluid) => ({ value: fluid, label: fluid }))}
+          value={{ value: fluidName2, label: fluidName2 }}
+          onChange={(selectedOption) => setFluidName2(selectedOption.value)}
+        />
+      </div>
+      <div>
+        <label>Proporção do fluido 2:</label>
+        <input
+          type="number"
+          value={proportion2}
+          onChange={(e) => setProportion2(e.target.value)}
+          placeholder="Proporção do fluido 2"
+        />
+      </div>
+    </div>
+  </div>
+)}
 
-      {/* Se for mistura, adicionar os campos para a substância 2 */}
-      {isMixture && (
-        <>
-          <div>
-            <label>Nome do fluido 2:</label>
-            <Select
-              options={fluids.map((fluid) => ({ value: fluid, label: fluid }))}
-              value={{ value: fluidName2, label: fluidName2 }}
-              onChange={(selectedOption) => setFluidName2(selectedOption.value)}
-            />
-          </div>
 
-          <div>
-            <label>Proporção do fluido 2:</label>
-            <input
-              type="number"
-              value={proportion2}
-              onChange={(e) => setProportion2(e.target.value)}
-              placeholder="Proporção"
-            />
-          </div>
-        </>
-      )}
-
-      {/* Propriedades */}
       <div className="property-fields">
         <div>
           <label>Propriedade #1:</label>
@@ -141,7 +148,6 @@ const CoolPropForm = ({
         </div>
       </div>
 
-      {/* Botão para calcular */}
       <div className="button-container">
         <button onClick={onCalculate}>Calcular</button>
       </div>
